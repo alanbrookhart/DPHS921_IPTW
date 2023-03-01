bfc5f8bca0fec26b00c2a1d1ce5ecf0b7173f7da
M Alan Brookhart <abrookhart@tgtrwe-lt-0077.lan>
2023-02-28 23:59:42 -0500

    fix some text

--- data_config.R
+++ data_config.R
@@ -8,7 +8,7 @@ data_list <- list(`New Users of NSAIDs` = 'ns', `New Users of Statins` = 'sta')
 
 data_descript <- list(
   ns = "This cohort is built from the 2005-2009 public use files from National Ambulatory Medical Care Data Survey (NAMCS). The NAMCS survey covers over 20,000 visits per year and includes data on patient demographics, comorbidities, physician and practice characteristics, and treatment received, including medications. Currently, medications are classified using the Multum Lexicon. Each year a few variables are added to, deleted from, or recoded in the public use dataset.
-From these data, we identify new users of either NSAID or Cox-2 selective non-steroidal antiinflammatory drugs (NSAIDs). The data are augmented with a simulated peptic ulcer disease outcome. Variables were selected for this extract based on relevance to the example analyses and on availability for most or all of the 2005-2009 time range.  For more information, see https://github.com/alanbrookhart/NAMCS#codebook"
+From these data, we identify new users of either a non-selective non-steroidal antiinflammatory drug (NSAID) or Cox-2 selective NSAID. The data are augmented with a simulated peptic ulcer disease outcome. Variables were selected for this extract based on relevance to the example analyses and on availability for most or all of the 2005-2009 time range.  For more information, see https://github.com/alanbrookhart/NAMCS#codebook"
   , 
   sta = "To be added."
   )
@@ -30,5 +30,5 @@ default_treatment = list(
 
 default_outcome = list(
   ns = "incident_pud",
-  sta  = "aspirin_use"
+  sta  = "cv_indicator"
 )
