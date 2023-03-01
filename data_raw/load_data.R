bc3b5bc93266ec08f40925fe2abd6e3fe2d2574d
M Alan Brookhart <abrookhart@TGTRWE-LT-0077.local>
2023-03-01 10:08:55 -0500

    updating data and moving files

--- data_raw/load_data.R
+++ data_raw/load_data.R
@@ -1,8 +1,8 @@
 
 # this files loads the NAMCS data files and places them in ./data
 
-devtools::install_github("alanbrookhart/NAMCS")
-library(NAMCS)
+#devtools::install_github("alanbrookhart/NAMCS")
+#library(NAMCS)
 
 saveRDS(ns, "data_raw/ns.rds")
 saveRDS(sta, "data_raw/sta.rds")
